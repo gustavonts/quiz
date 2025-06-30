a2b34ba5211068fa48a0f251ae96a2fb7dca5e01
Gustavo Guedes <gugaguedes11@gmail.com>
2025-06-30 20:47:55 -0300

    aula 145

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,32 +10,53 @@ const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
   RespostaModel.certa('Preta'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api'
+
 export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock)
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock)
+  const [respostasCertas, setRespostasCertas] = useState<number>(0)
+  
+  async function carregarIdsDasQuestoes(){
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
 
-  function questaoRespondida(questao : QuestaoModel){
+  async function carregarQuestao(idQuestao: number){
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json()
+    debugger
+    console.log(json.respostas)
+    const novaQuestao = QuestaoModel.criarUsandoObjeto(json)
+    setQuestao(novaQuestao)
+  }
 
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
+
+
+  useEffect(() => {
+    carregarIdsDasQuestoes()
+  }, [])
+
+  function questaoRespondida(questaoRespondida : QuestaoModel){
+    setQuestao(questaoRespondida)
+    const acertou = questaoRespondida.acertou
+    setRespostasCertas(respostasCertas + (acertou ? 1 : 0))
   }
   
   function irPraProximoPasso(){
-
+    
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
-        <Questionario
-          questao={questao}
-          ultima={true}
-          questaoRespondida={questaoRespondida}
-          irPraProximoPasso={irPraProximoPasso}
-        />
-    </div>
-    
+    <Questionario
+      questao={questao}
+      ultima={false}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso}
+    />
   )
 }
